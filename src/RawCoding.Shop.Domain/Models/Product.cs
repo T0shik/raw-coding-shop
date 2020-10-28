2cbedafaa6ddc7e933f99df6c82df2465f3c6e37
T0shik <antonwieslander@hotmail.com>
2020-10-28 21:10:40 +0000

    final todos

--- src/RawCoding.Shop.Domain/Models/Product.cs
+++ src/RawCoding.Shop.Domain/Models/Product.cs
@@ -4,6 +4,7 @@ namespace RawCoding.Shop.Domain.Models
 {
     public class Product
     {
+        // todo: staged / published
         public int Id { get; set; }
         public string Name { get; set; }
         public string Series { get; set; }
